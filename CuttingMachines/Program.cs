40f9f8d8983881cc210b1aac4481995d7e8673d8
Borja San Juan <148839774+borjasjca@users.noreply.github.com>
2023-10-25 17:02:14 +0200

    Add files via upload

---
+++ CuttingMachines/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Net;
+//using System.Net.Http;
+//using System.Net.Http.Headers;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using CuttingMachines.Models;
+
+
+namespace CuttingMachines
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            ConsoleKeyInfo key;
+
+            Console.WriteLine("Listing cutting machines:");
+            Console.WriteLine("* Press number 2 for filtering by 2D technology");
+            Console.WriteLine("* Press number 3 for filtering by 3D technology");
+            Console.WriteLine("* Press any other key for no filtering\r\n");
+
+            key = Console.ReadKey(true);
+
+            GetCuttingMachines(key.KeyChar);
+
+            Console.WriteLine("\r\nPress (ENTER) to quit");
+            Console.ReadLine();
+        }
+
+        public static void GetCuttingMachines(char IdTech)
+        {
+            System.Net.ServicePointManager.SecurityProtocol = System.Net.SecurityProtocolType.Tls12;
+
+            var url = $"https://app-academy-neu-codechallenge.azurewebsites.net/api/cut/";
+
+            if (IdTech.Equals('2') || IdTech.Equals('3') )
+                url += IdTech;
+
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+            request.Accept = "application/json";
+
+            string username = "lantekacademy";
+            string password = ":cPIi<kyF(=5OXc";
+
+            string authInfo = username + ":" + password;
+            authInfo = Convert.ToBase64String(Encoding.Default.GetBytes(authInfo));
+            request.Headers["Authorization"] = "Basic " + authInfo;
+
+            try
+            {
+                using (WebResponse response = request.GetResponse())
+                {
+                    using (Stream strReader = response.GetResponseStream())
+                    {
+                        if (strReader == null) return;
+                        using (StreamReader objReader = new StreamReader(strReader))
+                        {
+                            List<CuttingMachine> machines;
+                            string responseBody = objReader.ReadToEnd();
+                            machines = JsonConvert.DeserializeObject<List<CuttingMachine>>(responseBody);
+
+                            foreach (CuttingMachine machine in machines)
+                            {
+                                Console.WriteLine($"Name: {machine.Name}\tManufacturer: {machine.Manufacturer}\tCutting Technology: {machine.CuttingTechnology}D");
+                            }
+                        }
+                    }
+                }
+            }
+            catch (WebException ex)
+            {
+                // Handle error
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.InnerException);
+            }
+            catch (Exception ex)
+            {
+                // Handle error
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.InnerException);
+            }
+        }
+    }
+}
---
+++ Test/CuttingMachinesTest.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CuttingMachines;
+using System;
+using System.IO;
+using System.Text;
+using System.Net;
+using CuttingMachines.Models;
+
+namespace CuttingMachines.Tests
+{
+    [TestClass()]
+    public class CuttingMachinesTest
+    {
+        private HttpWebRequest request;
+        private HttpWebResponse response;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            System.Net.ServicePointManager.SecurityProtocol = System.Net.SecurityProtocolType.Tls12;
+
+            var url = $"https://app-academy-neu-codechallenge.azurewebsites.net/api/cut/";
+
+            request = (HttpWebRequest)WebRequest.Create(url);
+
+            string username = "lantekacademy";
+            string password = ":cPIi<kyF(=5OXc";
+
+            string authInfo = username + ":" + password;
+            authInfo = Convert.ToBase64String(Encoding.Default.GetBytes(authInfo));
+            request.Headers["Authorization"] = "Basic " + authInfo;
+            request.Method = "GET";
+            request.Accept = "application/json";
+        }
+
+        [TestCleanup]
+        public void Dispose()
+        {
+            response.Dispose();
+        }
+
+        [TestMethod()]
+        public void GetResponseTest()
+        {
+            response = (HttpWebResponse)request.GetResponse();
+
+            Assert.IsTrue(response.StatusCode.Equals(HttpStatusCode.OK));
+        }
+
+        [TestMethod()]
+        public void RetrievingDataTest()
+        {
+           string responseBody;
+
+            using (response = (HttpWebResponse)request.GetResponse())
+            {
+                using (Stream strReader = response.GetResponseStream())
+                {
+                    if (strReader == null) return;
+                    using (StreamReader objReader = new StreamReader(strReader))
+                    {
+                        responseBody = objReader.ReadToEnd();
+                    }
+                }
+            }
+
+            Assert.IsTrue(responseBody.Length > 0);
+        }
+    }
+}
